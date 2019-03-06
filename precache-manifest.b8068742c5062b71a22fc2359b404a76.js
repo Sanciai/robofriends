10ab2ff1a47b39f4569aa2d02c46cca7d90bdfe8
Tomas Vainoras <vainoras@gmail.com>
2019-03-06 10:37:47 +0200

    uploading website

---
+++ precache-manifest.b8068742c5062b71a22fc2359b404a76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f4c4a0c49692fe422f09",
+    "url": "/static/js/main.f4c4a0c4.chunk.js"
+  },
+  {
+    "revision": "b8c50535682a32983b0d",
+    "url": "/static/js/2.b8c50535.chunk.js"
+  },
+  {
+    "revision": "f4c4a0c49692fe422f09",
+    "url": "/static/css/main.a3beab9c.chunk.css"
+  },
+  {
+    "revision": "b8c50535682a32983b0d",
+    "url": "/static/css/2.f9ef29cf.chunk.css"
+  },
+  {
+    "revision": "1caca1c0c1b83deffe6bb8c7c7240b9a",
+    "url": "/index.html"
+  }
+];
